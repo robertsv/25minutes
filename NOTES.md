19bd8f62029e1e825c4abfe85206c0184a146efb
Roberts Vartins <roberts.vartins@gmail.com>
2020-03-27 22:41:43 +0200

    Test for DataService

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+ng test --include=src/app/shared/data.service.spec.ts
+https://christianlydemann.com/all-you-need-to-know-about-mocking-in-angular-tests/
+https://medium.com/@shashankvivek.7/testing-services-in-angular-karma-ed49f8d5b264
--- src/app/shared/data.service.spec.ts
+++ src/app/shared/data.service.spec.ts
@@ -1,12 +1,65 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { DataService } from './data.service';
+import {DataService} from './data.service';
+import {StorageService} from './storage.service';
+import {Task} from './task';
 
 describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: DataService = TestBed.get(DataService);
-    expect(service).toBeTruthy();
+  let storageServiceSpy;
+  beforeEach(() => {
+    storageServiceSpy = jasmine.createSpyObj('StorageService', ['loadTasksStorage', 'saveTasksInStorage']);
+    storageServiceSpy.loadTasksStorage.and.callFake(() => {
+      return [
+        new Task('Test task #1', false, false),
+        new Task('Test task #2', false, false),
+      ];
+    });
+    // storageServiceSpy.loadTasksStorage = () => [new Task('Test task1', false, false)];
+    TestBed.configureTestingModule({
+      providers: [DataService,
+        {provide: StorageService, useValue: storageServiceSpy}
+      ]
+    });
   });
+
+  it('should return tasks', () => {
+    const dataService: DataService = TestBed.get(DataService);
+    expect(dataService.getTasks()).toEqual([
+      new Task('Test task #1', false, false),
+      new Task('Test task #2', false, false)]
+    );
+  });
+
+  it('should add task', () => {
+    const dataService: DataService = TestBed.get(DataService);
+    dataService.addTask('Test task #3');
+    expect(dataService.getTasks()).toEqual([
+      new Task('Test task #1', false, false),
+      new Task('Test task #2', false, false),
+      new Task('Test task #3', false, false)]
+    );
+    expect(storageServiceSpy.saveTasksInStorage).toHaveBeenCalled();
+  });
+
+  it('should mark task', () => {
+    const dataService: DataService = TestBed.get(DataService);
+    dataService.checkOrUncheckTask(0);
+    dataService.checkOrUncheckTask(0);
+    dataService.checkOrUncheckTask(1);
+    expect(dataService.getTasks()).toEqual([
+      new Task('Test task #1', false, false),
+      new Task('Test task #2', true, false)]
+    );
+    expect(storageServiceSpy.saveTasksInStorage).toHaveBeenCalled();
+  });
+
+  it('should delete task', () => {
+    const dataService: DataService = TestBed.get(DataService);
+    dataService.deleteTask(0);
+    expect(dataService.getTasks()).toEqual([
+      new Task('Test task #2', false, false)]
+    );
+    expect(storageServiceSpy.saveTasksInStorage).toHaveBeenCalled();
+  });
+
 });
