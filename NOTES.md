9b454840a7864cd025e0f5d76a7702d1a3443d2e
Roberts Vartins <roberts.vartins@gmail.com>
2020-03-29 13:22:48 +0300

    Tasks for date pipe

--- NOTES.md
+++ NOTES.md
@@ -2,6 +2,9 @@ ng test --include=src/app/shared/experimenting.spec.ts
 ng test --include=src/app/shared/data.service.spec.ts
 ng test --include=src/app/shared/min-validator.directive.spec.ts
 ng test --include=src/app/shared/max-validator.directive.spec.ts
+ng test --include=src/app/shared/minutes-seconds.pipe.spec.ts
+ng test --include=src/app/shared/notification.service.spec.ts
+
 
 https://angular.io/guide/testing
 https://christianlydemann.com/all-you-need-to-know-about-mocking-in-angular-tests/
--- src/app/shared/minutes-seconds.pipe.spec.ts
+++ src/app/shared/minutes-seconds.pipe.spec.ts
@@ -1,8 +1,37 @@
-import { MinutesSecondsPipe } from './minutes-seconds.pipe';
+import {MinutesSecondsPipe} from './minutes-seconds.pipe';
 
 describe('MinutesSecondsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MinutesSecondsPipe();
-    expect(pipe).toBeTruthy();
+  let pipe;
+  beforeEach(() => {
+    pipe = new MinutesSecondsPipe();
   });
+
+  it('should transform 0', () => {
+    expect(pipe.transform(0)).toBe('00:00');
+  });
+
+  it('should transform 1', () => {
+    expect(pipe.transform(1)).toBe('00:01');
+  });
+
+  it('should transform 60', () => {
+    expect(pipe.transform(60)).toBe('01:00');
+  });
+
+  it('should transform 61', () => {
+    expect(pipe.transform(61)).toBe('01:01');
+  });
+
+  it('should transform 3599', () => {
+    expect(pipe.transform(3599)).toBe('59:59');
+  });
+
+  it('should transform 3600', () => {
+    expect(pipe.transform(3600)).toBe('60:00');
+  });
+
+  it('should transform 3661', () => {
+    expect(pipe.transform(3661)).toBe('61:01');
+  });
+
 });
